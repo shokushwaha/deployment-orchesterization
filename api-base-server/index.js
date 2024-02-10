8c1b95f421b35bd0e7c0d9fffdd75e52b128918c
Shobhit Kushwaha <shobhitkushwaha1406@gmail.com>
2024-02-11 00:49:44 +0530

    initial

---
+++ api-base-server/index.js
@@ -0,0 +1,84 @@
+const express = require('express')
+const { generateSlug } = require('random-word-slugs')
+const { ECSClient, RunTaskCommand } = require('@aws-sdk/client-ecs')
+const { Server } = require('socket.io')
+const Redis = require('ioredis')
+
+const app = express()
+const PORT = process.env.API_PORT
+
+const subscriber = new Redis('')
+
+const io = new Server({ cors: '*' })
+
+io.on('connection', socket => {
+    socket.on('subscribe', channel => {
+        socket.join(channel)
+        socket.emit('message', `Joined ${channel}`)
+    })
+})
+
+io.listen(9002, () => console.log('Socket Server 9002'))
+
+const ecsClient = new ECSClient({
+    region: process.env.ECS_CLIENT_REGION,
+    credentials: {
+        accessKeyId: process.env.ECS_CLIENT_ACCESS_KEY_ID,
+        secretAccessKey: process.env.ECS_CLIENT_SECRET_ACCESS_KEY
+    }
+})
+
+const config = {
+    CLUSTER: process.env.CLUSTER,
+    TASK: process.env.TASK
+}
+
+app.use(express.json())
+
+app.post('/build', async (req, res) => {
+    const { gitURL, slug } = req.body
+    const projectSlug = slug ? slug : generateSlug()
+
+    const command = new RunTaskCommand({
+        cluster: config.CLUSTER,
+        taskDefinition: config.TASK,
+        launchType: 'FARGATE',
+        count: 1,
+        networkConfiguration: {
+            awsvpcConfiguration: {
+                assignPublicIp: 'ENABLED',
+                subnets: process.env.SUBNETS,
+                securityGroups: process.env.SECURITY_GROUPS
+            }
+        },
+        overrides: {
+            containerOverrides: [
+                {
+                    name: 'builder-image',
+                    environment: [
+                        { name: 'GIT_REPOSITORY__URL', value: gitURL },
+                        { name: 'PROJECT_ID', value: projectSlug }
+                    ]
+                }
+            ]
+        }
+    })
+
+    await ecsClient.send(command);
+
+    return res.json({ status: 'queued', data: { projectSlug, url: `http://${projectSlug}.localhost:8000` } })
+
+})
+
+async function initRedisSubscribe() {
+    console.log('Subscribed to logs....')
+    subscriber.psubscribe('logs:*')
+    subscriber.on('pmessage', (pattern, channel, message) => {
+        io.to(channel).emit('message', message)
+    })
+}
+
+
+initRedisSubscribe()
+
+app.listen(PORT, () => console.log(`API Server Running..${PORT}`))
